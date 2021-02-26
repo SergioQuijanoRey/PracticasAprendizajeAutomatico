588d4211c089404ba986fb043bef13179e85c237
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-02-26 11:20:48 +0100

    feat: done with exercise1

--- Practica0/ejercicio1.py
+++ Practica0/ejercicio1.py
@@ -3,6 +3,7 @@
 from sklearn import datasets
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as pltcols
 
 
 def run():
@@ -21,11 +22,11 @@ def read_iris_data():
 
     # Tomamos los datos del dataset
     # Esta es la parte en la que copio codigo de la fuente mencionada
-    iris = datasets.load_iris()
+    iris_dataset = datasets.load_iris()
 
     # Separamos caracteristicas de las clases
-    data = iris.data
-    classes = iris.target
+    data = iris_dataset.data
+    classes = iris_dataset.target
 
     # Nos quedamos solo con la primera y tercera caracteristica que corresponden
     # a los indices 0 y 2
@@ -41,5 +42,24 @@ def plot_iris_dataset(data, classes):
     x_values = [data[x][0] for x in range(len(data))]
     y_values = [data[x][1] for x in range(len(data))]
 
-    # c = classes colorea los puntos segun las clases a las que pertenezcan
-    plt.scatter(x_values, y_values, c = classes)
+    # Tomamos la figura y ejes por separado en vez de hacer manipulaciones directas
+    # para poder poner leyendas y otras operaciones complejas
+    _, ax = plt.subplots()
+
+    # Coloreamos las distintas clases segun los colores que se nos ha especificado
+    # La parte de hacer ListedColormap la saco de: https://stackoverflow.com/questions/12487060/
+    colormap = ['orange', 'black', 'green']
+    scatter = ax.scatter(x_values, y_values, c=classes,
+                         cmap=pltcols.ListedColormap(colormap))
+
+    # Asignamos las etiquetas a los colores
+    # Consultado de la documentacion oficial de matplotlib
+    # En concreto: https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/scatter_with_legend.html#automated-legend-creation
+    legend = ax.legend(*scatter.legend_elements(), title="Clases", loc = "upper left")
+    ax.add_artist(legend)
+
+    # Ponemos un titulo a la grafica
+    plt.title("Grafica de caracteristicas y sus clases")
+
+    # Mostramos el grafico
+    plt.show()
