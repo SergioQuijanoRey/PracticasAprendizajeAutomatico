68436c11e9077cfd3f07eb852ddfeacdbf25398e
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-03-15 18:13:09 +0100

    feat: exercise 1 part 3 done, but weird behaviour

--- Práctica1/main.py
+++ Práctica1/main.py
@@ -8,33 +8,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-print("Buenas tardes")
-
-
 # Funciones Auxiliares / Comunes
 #===============================================================================
 def wait_for_user_input():
     input("Pulse ENTER para continuar...")
 
-def gradient_descent(starting_point, loss_function, gradient, learning_rate: float = 0.001, max_iterations: int = 100_000, target_error: float = 1e-10):
+# Algoritmos
+#===============================================================================
+def gradient_descent(starting_point, loss_function, gradient, learning_rate: float = 0.001, max_iterations: int = 100_000, target_error: float = 1e-10, verbose: bool = False):
+    """
+    Implementa el algoritmo de batch gradient descent
+
+    Si verbose == true, entonces guardamos los errores y soluciones obtenidas en cada iteracion
+    """
     current_solution = starting_point
     current_iteration = 0
 
-    error_at_iteration = []
+    # Guardamos errores y soluciones de las iteraciones cuando verbose == True
+    error_at_iteration = None
+    solution_at_iteration = None
+    if verbose == True:
+        error_at_iteration = [loss_function(current_solution[0], current_solution[1])]
+        solution_at_iteration = [starting_point]
 
     while current_iteration < max_iterations and loss_function(current_solution[0], current_solution[1]) > target_error:
         current_solution = current_solution - learning_rate * gradient(current_solution[0], current_solution[1])
         current_iteration = current_iteration + 1
-        error_at_iteration.append(loss_function(current_solution[0], current_solution[1]))
 
-    return current_solution, current_iteration, error_at_iteration
+        if verbose == True:
+            error_at_iteration.append(loss_function(current_solution[0], current_solution[1]))
+            solution_at_iteration.append(current_solution)
 
+    return current_solution, current_iteration, np.array(error_at_iteration), np.array(solution_at_iteration)
+
+def gradient_descent_and_plot_error(starting_point, loss_function, gradient, learning_rate: float = 0.001, max_iterations: int = 100_000, target_error: float = 1e-10):
+    """
+    Ejecutamos el gradiente descendente y mostramos la grafica de la evolucion del error
+    Para no repetir muchas veces el mismo codigo en el que lanzamos el algoritmo y
+    mostramos la grafica de la evolucion del error
+    """
+
+    print(f"Corriendo el algoritmo de descenso del gradiente para eta = {learning_rate}")
+    weights, iterations, error_at_iteration, solution_at_iteration = gradient_descent(starting_point, loss_function, gradient, learning_rate, max_iterations, target_error, verbose = True)
+
+    # Mostramos la grafica de descenso del error
+    Y = error_at_iteration
+    X = np.arange(0, len(Y))
+
+    plt.title(f"Evolucion del error para eta = {learning_rate}")
+    plt.xlabel("Iteracion")
+    plt.ylabel("Error")
+    plt.plot(X, Y)
+    plt.show()
+    wait_for_user_input()
+
+    # Por si necesitamos realizar otras operaciones con los resultados
+    return weights, iterations, error_at_iteration, solution_at_iteration
+
+# TODO -- hay que implementar esta funcion
+def plot_gradient_space(gradient, x_interval, y_interval):
+    pass
 
 # Ejercicio 1
 #===============================================================================
-def ejercicio1():
-    print("Ejecutando ejercicio 1")
-
+def ejercicio1_apartado2():
     # Definimos la funcion de coste
     # Me quedo con el cuerpo del cuadrado porque lo usaremos en las derivadas parciales
     inside = lambda u, v: np.power(u, 3) * np.exp(v - 2) - 2.0 * np.power(v, 2) * np.exp(-u)
@@ -48,32 +85,110 @@ def ejercicio1():
     # Gradiente de la funcion de coste
     gradient = lambda u, v: np.array([dEu(u, v), dEv(u, v)])
 
-    # Mostramos la expresion del gradiente
-    # TODO -- escribir la funcion del gradiente
+    # Mostramos la expresion de la funcion de coste y la expresion del gradiente
+    print("La funcion de coste es:")
+    print("\t(u³ * exp(v−2) −2v² exp(−u))²")
+    print("La derivada parcial respecto de u es:")
+    print("\t2 * (u³ * exp(v−2) −2v² exp(−u)) * (3u² * exp(v-2) + 2v²exp(-u)")
+    print("La derivada parcial respecto v es:")
+    print("\t2* (u³ * exp(v−2) −2v² exp(−u)) * (u³ * exp(v-2) -4vexp(-u))")
+    print("")
+    wait_for_user_input()
 
     # Parametros para el gradiente descendente
     learning_rate = 0.1
     max_iterations = 10000000000
     target_error = 1e-14
-    starting_point = np.array([1.0,1.0])
+    starting_point = np.array([1.0, 1.0])
 
-    # Lanzamos el descenso y mostramos los resultados
-    weights, iterations, error_at_iteration = gradient_descent(starting_point, E, gradient, learning_rate, max_iterations, target_error)
+    # Lanzamos el descenso y mostramos la grafica de evolucion del error
+    weights, iterations, error_at_iteration, solution_at_iteration = gradient_descent_and_plot_error(starting_point, E, gradient, learning_rate, max_iterations, target_error)
 
-    Y = error_at_iteration
-    X = np.arange(0, len(Y))
-
-    plt.title("Evolucion del error")
-    plt.xlabel("Iteracion")
-    plt.ylabel("Error")
-    plt.plot(X, Y)
-    plt.show()
+    # Mostramos algunos datos numericos del resultado
+    print("Resultados:")
+    print(f"\tNumero de iteraciones: {iterations}")
+    print(f"\tPesos encontrados: {weights}")
     wait_for_user_input()
 
-    print(f"Numero de iteraciones: {iterations}")
-    print(f"Pesos encontrados: {weights}")
+    # Cuantas iteraciones tarda el algoritmo en obtener un valor de E inferior a 10e-14
+    # Hacemos indixes[0] para quedarnos con la lista de indices que devuelve, e
+    # indixes[0][0] para quedarnos con el primer elemento de dicha lista
+    indixes = np.where(error_at_iteration < 10e-14)
+    first_index_under_error = indixes[0][0]
+    print(f"La primera iteracion en la que el error esta por debajo de 10e-14 es: {first_index_under_error}")
+    print(f"Las primeras coordenadas que estan por debajo de ese error: {solution_at_iteration[first_index_under_error]}")
+    print("")
     wait_for_user_input()
 
+def ejercicio1_apartado3():
+    # Funcion de perdida que nos dan y calculo sus derivadas parciales
+    # No muestro por pantalla las expresiones porque el enunciado no lo pide
+    f = lambda x, y: np.power(x + 2.0, 2.0) + 2.0 * np.power(y - 2.0, 2.0) + 2 * np.sin(2.0 * np.pi * x) * np.sin(2.0 * np.pi * y)
+    dfx = lambda x, y: 2.0 * (x - 2.0) + 4.0 * np.pi * np.sin(2.0 * np.pi * y) * np.cos(2.0 * np.pi * x)
+    dfy = lambda x, y: 4.0 * (y - 2.0) + 4.0 * np.pi * np.sin(2.0 * np.pi * x) * np.cos(2.0 * np.pi * y)
+    gradient = lambda x, y: np.array([dfx(x, y), dfy(x, y)])
+
+    # Parametros para el gradiente descendiente
+    # No se especifica el error que hay que alcanzar asi que lo pongo a cero
+    # para que se consuman las iteraciones
+    learning_rate = 0.01
+    max_iterations = 50
+    target_error = 0
+    starting_point = np.array([-1.0,1.0])
+
+    # Lanzamos el descenso y mostramos la grafica con los resultados
+    gradient_descent_and_plot_error(starting_point, f, gradient, learning_rate, max_iterations, target_error)
+
+    # Realizamos el mismo proceso pero modificando el valor del learning rate
+    learning_rate = 0.1
+    gradient_descent_and_plot_error(starting_point, f, gradient, learning_rate, max_iterations, target_error)
+
+    # Ahora buscamos los errores minimos y valores de la solucion cuando partimos
+    # desde distintos valores de partida
+
+    # Como no se especifica nada en el enunciado del ejercicio, establezco el
+    # learning rate, el numero maximo de iteraciones y el target_error
+    learning_rate = 0.01
+    max_iterations = 10_000
+    target_error = 1e-15
+
+    # Muestro explicitamente que estos valores los he fijado yo al no tener indicaciones
+    print("Fijo los siguientes parametros para el gradiente descendente:")
+    print(f"\tlearning_rate: {learning_rate}")
+    print(f"\tmax_iterations: {max_iterations}")
+    print(f"\ttarget_error: {target_error}")
+    print("")
+    wait_for_user_input()
+
+    # Vector con los puntos de partida que se nos piden
+    # Para recorrerlo comodamente en un bucle y evitar repitir codigo
+    starting_points = [
+        [-0.5, -0.5],
+        [1, 1],
+        [2, 1],
+        [-2, 1],
+        [-3, 3],
+        [-2, 2]
+    ]
+
+    for starting_point in starting_points:
+        starting_point = np.array(starting_point)
+        weights, iterations, error_at_iteration, solution_at_iteration = gradient_descent_and_plot_error(starting_point, f, gradient, learning_rate, max_iterations, target_error)
+        print(f"Resultados para starting_point: {starting_point}")
+        print(f"\t(x, y): {weights}")
+        print(f"\tError final: {error_at_iteration[-1]}")
+        wait_for_user_input()
+
+def ejercicio1():
+    print("Ejecutando ejercicio 1")
+
+    print("Apartado 2)")
+    print("=" * 80)
+    ejercicio1_apartado2()
+
+    print("Apartado 3)")
+    print("=" * 80)
+    ejercicio1_apartado3()
 
 # Corremos todos los ejercicios
 #===============================================================================
