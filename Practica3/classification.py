2cb9b69e2cf2ab52da71208fcfeb46a4ceeb3ad3
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-05-05 18:39:43 +0200

    feat: empiezo con el codigo base para la tercera practica

---
+++ Practica3/classification.py
@@ -0,0 +1,6 @@
+"""
+Author:
+    - Sergio Quijano Rey
+    - sergioquijano@correo.ugr.es
+Practica 3 - Problema de clasificacion
+"""
---
+++ Practica3/core.py
@@ -0,0 +1,10 @@
+"""
+Author:
+    - Sergio Quijano Rey
+    - sergioquijano@correo.ugr.es
+Practica 3 - Funciones comunes a los dos problemas: el de clasificacion y el de regresion
+"""
+
+def wait_for_user_input():
+    """Esperamos a que el usuario pulse una tecla para continuar"""
+    input("Pulse una tecla para CONTINUAR...")
